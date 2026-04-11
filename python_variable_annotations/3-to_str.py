8c8ed7eb24de0da6b7f507411918141d403d8ff8
elliot <charlet.elliot@gmail.com>
2026-04-11 13:05:10 +0200

    transform float to str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/boin/env python3
+"""A type-annotated function to_str that takes
+a float n as argument and returns the string
+representation of the float"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of n."""
+    return str(n)
