c3777a01e0c892bf93ab766c015059ab2dfcbad0
elliot <charlet.elliot@gmail.com>
2026-04-11 21:25:16 +0200

    an async routine called wait_n that takes in 2 int arguments (in this order): n and max_delay. You will spawn wait_random n times with the specified max_delay.

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""An async routine called wait_n that takes in 2 int
+arguments (in this order): n and max_delay. You will
+spawn wait_random n times with the specified max_delay.
+"""
+
+import asyncio
+import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Returns a list of all the delays (float values) of wait_random
+    called n times with the specified max_delay.
+    """
+    delays = []
+    for i in range(n):
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+    return delays
