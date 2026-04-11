3e96aed13c0182dbec2d63819fbda4e163ec6de5
elliot <charlet.elliot@gmail.com>
2026-04-11 13:00:50 +0200

    change the pycode style

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""A  type-annotated function floor 
-which takes a float n as argument and 
+"""A  type-annotated function floor
+which takes a float n as argument and
 returns the floor of the float"""
 
 
