93b358d0884d3f97de12f8afa4337ec6f844d051
elliot <charlet.elliot@gmail.com>
2026-04-11 18:00:16 +0200

    append the Tuple from typing

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -7,9 +7,9 @@ square of the int/float v and should be annotated
 as a float"""
 
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple containing k and the square of v as a float."""
     return (k, float(v ** 2))
