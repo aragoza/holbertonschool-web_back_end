67f33de8a9ff6a489d8221f571c533f864237d66
elliot <charlet.elliot@gmail.com>
2026-04-26 23:33:22 +0200

    update a topic

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+changes all topics of a school document based on the name:
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update mongo_collection name with topics
+    """
+
+    mongo_collection.update_many({"name" : name},{ "$set" : { "topics" : topics} } )
